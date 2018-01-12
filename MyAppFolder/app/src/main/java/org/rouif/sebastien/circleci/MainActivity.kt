906d6a4e9be6fa4afdfb82648cbdfe24bbca46eb
Sebastien Rouif <sebastien.rouif@gmail.com>
2018-01-12 21:16:58 +0000

    [setup] Added CircleCi + crashlytics

--- MyAppFolder/app/src/main/java/org/rouif/sebastien/circleci/MainActivity.kt
+++ MyAppFolder/app/src/main/java/org/rouif/sebastien/circleci/MainActivity.kt
@@ -1,12 +1,16 @@
 package org.rouif.sebastien.circleci
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        force_crash.setOnClickListener {
+            throw RuntimeException("This is a crash");
+        }
     }
 }
